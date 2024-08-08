84b9a7bbe5c722314fd404f0aaf46a67142b6ceb
Artem Mysiv <amysiv@f0r3s21.codam.nl>
2024-08-08 15:38:31 +0200

    last

--- main.c
+++ main.c
@@ -6,11 +6,29 @@
 /*   By: amysiv <amysiv@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 12:56:16 by amysiv            #+#    #+#             */
-/*   Updated: 2024/08/08 14:47:11 by amysiv           ###   ########.fr       */
+/*   Updated: 2024/08/08 14:57:51 by amysiv           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	check_built_in(char *command)
+{
+		if (ft_strncmp(command, "pwd", 4) == 0)
+			ft_pwd();
+		//if (ft_strncmp(command, "cd", 3))
+		//	//todo
+		//if (ft_strncmp(command, "echo", 5))
+		//	//todo
+		//if (ft_strncmp(command, "export", 7))
+		//	//todo
+		//if (ft_strncmp(command, "unset", 6))
+		//	//todo;
+		//if (ft_strncmp(command, "env", 4))
+		//	//todo;
+		//if (ft_strncmp(command, "exit", 5))
+		//	//todo;
+}
 
 int main()
 {
@@ -22,13 +40,9 @@ int main()
 		if (command == NULL)
 			break;
 		add_history(command);
-		if (ft_strncmp(command, "pwd", 4) == 0)
-			ft_pwd();
-		else
-			perror("ERROR in getcwd()");
+		check_built_in(command);
 		
 	}
-
 
 	return (0);
 }
